7b1276fa4a4748d647323f4ee6ed3ebb9c6722ab
BeYkeRYkt <beykerykt@gmail.com>
2018-06-28 00:16:12 +0700

    ChilQ: Rename item_card_view to 'specific' name
    * This CardView is used only in TaskRecyclerAdapter. 
    Rename it to a more specific name, so as not to be
    confused in the future.
    
    Change-Id: I0d516c493501b0d5b7ac53d213cde8b17489fe8b

--- app/src/main/java/com/example/zz/chilq/cardview/adapters/TaskRecyclerAdapter.java
+++ app/src/main/java/com/example/zz/chilq/cardview/adapters/TaskRecyclerAdapter.java
@@ -23,7 +23,7 @@ public class TaskRecyclerAdapter extends RecyclerView.Adapter<TaskRecycleViewHol
     public TaskRecycleViewHolder onCreateViewHolder(ViewGroup parent,
                                                     int viewType) {
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_card_view, parent, false);
+                .inflate(R.layout.item_tasks_card_view, parent, false);
 
         TaskRecycleViewHolder vh = new TaskRecycleViewHolder(v);
         return vh;
